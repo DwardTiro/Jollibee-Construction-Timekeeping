34c2c3756e2c3ad0010331148a3d5ad7948c5514
arvention <amc.antioquia@yahoo.com.ph>
2016-06-14 14:39:05 +0800

    controller logout

--- [PROSDEV] Jollibee Timekeeping/src/controller/Controller.java
+++ [PROSDEV] Jollibee Timekeeping/src/controller/Controller.java
@@ -12,6 +12,7 @@ import java.awt.event.MouseListener;
 public class Controller {
     
     private final String USERNAME_FIELD_DEFAULT = "Username";
+    private final String PASSWORD_FIELD_DEFAULT = "Password";
     private final String CARD_LOGIN = "panelLogin";
     private final String CARD_MAIN = "panelMain";
     
@@ -92,6 +93,20 @@ public class Controller {
             
         });
         
+        mainFrame.getPasswordfieldLoginPassword().addFocusListener(new FocusListener(){
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(String.copyValueOf(mainFrame.getPasswordfieldLoginPassword().getPassword()).equalsIgnoreCase(PASSWORD_FIELD_DEFAULT)){
+                    mainFrame.getPasswordfieldLoginPassword().setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {}
+            
+        });
+        
         mainFrame.getButtonLogin().addMouseListener(new MouseListener(){
 
             @Override
@@ -125,5 +140,10 @@ public class Controller {
         // backend logic here
         
         return login;
+    }
+    
+    private void logout(){
+        CardLayout cardLayout = (CardLayout) mainFrame.getLayout();
+        cardLayout.show(mainFrame, CARD_MAIN);
     }
 }
