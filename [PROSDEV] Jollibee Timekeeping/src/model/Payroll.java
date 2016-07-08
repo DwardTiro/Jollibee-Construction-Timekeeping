9711b9fbbb311448aebeee6a868a28e6581f2d8b
jetforce <jetvirtusio@hotmail.com>
2016-07-08 14:01:21 +0800

    Payrol

---
+++ [PROSDEV] Jollibee Timekeeping/src/model/Payroll.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Jet
+ */
+public class Payroll {
+    
+    public static void computeSalary(){
+        //PreparedStatement ps = DbConnection.getConnection().prepareStatement("");
+        
+        
+    }
+    
+    
+    
+}
