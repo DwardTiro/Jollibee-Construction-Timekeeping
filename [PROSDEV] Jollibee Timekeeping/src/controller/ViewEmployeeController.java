551e7673b1f27c6c47d782b32e9179d73243fe67
arvention <amc.antioquia@yahoo.com.ph>
2016-07-24 18:55:21 +0800

    push

--- [PROSDEV] Jollibee Timekeeping/src/controller/ViewEmployeeController.java
+++ [PROSDEV] Jollibee Timekeeping/src/controller/ViewEmployeeController.java
@@ -5,8 +5,6 @@ import gui.MainFrame;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.PreparedStatement;
