dc45b2903f80a99d28c0930bb8288607c6fc2306
jetforce <jetvirtusio@hotmail.com>
2016-08-08 22:09:13 +0800

    Patch edit add

--- [PROSDEV] Jollibee Timekeeping/src/controller/AddEmployeeController.java
+++ [PROSDEV] Jollibee Timekeeping/src/controller/AddEmployeeController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import gui.MainFrame;
@@ -14,58 +13,66 @@ import java.util.logging.Logger;
 
 import model.Employee;
 
-public class AddEmployeeController implements Listen, PanelChanger{
+public class AddEmployeeController implements Listen, PanelChanger {
 
     private static final AddEmployeeController addEmployeeController = new AddEmployeeController();
     private final MainFrame mainFrame;
-    
+
     private final String PANEL_NAME = "addEmployeePanel";
-    
-    private AddEmployeeController(){
+
+    private AddEmployeeController() {
         mainFrame = MainFrame.getInstance();
-        
+
         addListeners();
     }
-    
-    public static AddEmployeeController getInstance(){
+
+    public static AddEmployeeController getInstance() {
         return AddEmployeeController.addEmployeeController;
     }
-    
+
     @Override
     public final void addListeners() {
-        mainFrame.getButtonAddEmployee().addMouseListener(new MouseListener(){
+        mainFrame.getButtonAddEmployee().addMouseListener(new MouseListener() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                
-                String salary_value = mainFrame.getTextFieldAddEmployeeSalary().getText();
-                
-                if(!mainFrame.getTextFieldAddEmployeeIDNumber().getText().isEmpty() &&
-                   !mainFrame.getTextFieldAddEmployeeLastName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldAddEmployeeFirstName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldAddEmployeeMiddleName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldAddEmployeeSalary().getText().isEmpty()){
-                    
-                    if(tryAddEmployee(Integer.parseInt(mainFrame.getTextFieldAddEmployeeIDNumber().getText()),
-                            mainFrame.getTextFieldAddEmployeeLastName().getText(),
-                            mainFrame.getTextFieldAddEmployeeFirstName().getText(),
-                            mainFrame.getTextFieldAddEmployeeMiddleName().getText(),
-                            Double.parseDouble(salary_value))){
-                        // success: notify user
-                        PopBox.infoBox("Employee Added!","");
-                        
-                    } else{
-                        // failure: notify user
-                        PopBox.infoBox("Failed to add employee!","");
+                try {
+
+                    String salary_value = mainFrame.getTextFieldAddEmployeeSalary().getText();
+
+                    if (!mainFrame.getTextFieldAddEmployeeIDNumber().getText().isEmpty()
+                            && !mainFrame.getTextFieldAddEmployeeLastName().getText().isEmpty()
+                            && !mainFrame.getTextFieldAddEmployeeFirstName().getText().isEmpty()
+                            && !mainFrame.getTextFieldAddEmployeeMiddleName().getText().isEmpty()
+                            && !mainFrame.getTextFieldAddEmployeeSalary().getText().isEmpty()) {
+
+                        if (tryAddEmployee(Integer.parseInt(mainFrame.getTextFieldAddEmployeeIDNumber().getText()),
+                                mainFrame.getTextFieldAddEmployeeLastName().getText(),
+                                mainFrame.getTextFieldAddEmployeeFirstName().getText(),
+                                mainFrame.getTextFieldAddEmployeeMiddleName().getText(),
+                                Double.valueOf(salary_value))) {
+                            // success: notify user
+                            PopBox.infoBox("Employee Added!", "");
+
+                        } else {
+                            // failure: notify user
+                            PopBox.infoBox("Failed to add employee!", "");
+                        }
                     }
+
+                } catch (Exception ex) {
+                    PopBox.infoBox("Failed to add employee!", "");
                 }
+
             }
 
             @Override
-            public void mousePressed(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {
+            }
 
             @Override
-            public void mouseReleased(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {
+            }
 
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -76,41 +83,38 @@ public class AddEmployeeController implements Listen, PanelChanger{
             public void mouseExited(MouseEvent e) {
                 mainFrame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             }
-            
+
         });
     }
-    
+
     @Override
     public void showPanel() {
         CardLayout cardLayout = (CardLayout) mainFrame.getMainPanelCardPanel().getLayout();
         cardLayout.show(mainFrame.getMainPanelCardPanel(), PANEL_NAME);
     }
-    
+
     // returns true if successful in adding employee, false if there is a similar IDNumber in database
-    private boolean tryAddEmployee(int IDNumber, String lastName, String firstName, String middleName, double salary){
+    private boolean tryAddEmployee(int IDNumber, String lastName, String firstName, String middleName, double salary) throws SQLException {
         boolean tryAdd = false;
-        
+
         String mySQL_string = Employee.getAddEmployeeStatement();
-        
-        if(salary > 0){
-            try {
-                /*
-                PreparedStatement ps = DbConnection.getConnection().prepareStatement(mySQL_string);
-                ps.setInt(1, IDNumber);
-                ps.setString(2, firstName);
-                ps.setString(3, lastName);
-                ps.setString(4,middleName);
-                ps.setDouble(5, salary);
-                */
-                Employee.addDB(IDNumber, firstName, lastName, middleName, salary);
-                tryAdd= true; 
-            } catch (SQLException ex) {
-                Logger.getLogger(AddEmployeeController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+
+        if (salary > 0) {
+
+            /*
+             PreparedStatement ps = DbConnection.getConnection().prepareStatement(mySQL_string);
+             ps.setInt(1, IDNumber);
+             ps.setString(2, firstName);
+             ps.setString(3, lastName);
+             ps.setString(4,middleName);
+             ps.setDouble(5, salary);
+             */
+            Employee.addDB(IDNumber, firstName, lastName, middleName, salary);
+            tryAdd = true;
+
         }
-      
+
         // logic here
-        
         return tryAdd;
     }
 }
--- [PROSDEV] Jollibee Timekeeping/src/controller/EditEmployeeController.java
+++ [PROSDEV] Jollibee Timekeeping/src/controller/EditEmployeeController.java
@@ -21,65 +21,75 @@ import model.CalendarModel;
 import model.Employee;
 import model.EmployeeDetailsAuditTrail;
 
-public class EditEmployeeController implements Listen, PanelChanger{
+public class EditEmployeeController implements Listen, PanelChanger {
 
-    private static final EditEmployeeController editEmployeeController =  new EditEmployeeController();
+    private static final EditEmployeeController editEmployeeController = new EditEmployeeController();
     private final MainFrame mainFrame;
-    
+
     private final String PANEL_NAME = "editEmployeePanel";
-    
+
     private ArrayList<EmployeeDetailsAuditTrail> auditTrails;
     private ArrayList<EditEmployeeChangesPanel> auditTrailPanels;
-    
+
     private int viewID;
-    
-    
-    private EditEmployeeController(){
+
+    private EditEmployeeController() {
         mainFrame = MainFrame.getInstance();
-        
+
         auditTrails = new ArrayList<>();
         auditTrailPanels = new ArrayList<>();
-        
+
         addListeners();
     }
-    
-    public static EditEmployeeController getInstance(){
+
+    public static EditEmployeeController getInstance() {
         return EditEmployeeController.editEmployeeController;
     }
-    
+
     @Override
     public final void addListeners() {
-        mainFrame.getButtonEditEmployeeSaveChanges().addMouseListener(new MouseListener(){
+        mainFrame.getButtonEditEmployeeSaveChanges().addMouseListener(new MouseListener() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(!mainFrame.getTextFieldEditEmployeeIDNumber().getText().isEmpty() &&
-                   !mainFrame.getTextFieldEditEmployeeLastName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldEditEmployeeFirstName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldEditEmployeeMiddleName().getText().isEmpty() &&
-                   !mainFrame.getTextFieldEditEmployeeSalary().getText().isEmpty()){
-                    
-                    if(tryEditEmployee(Integer.parseInt(mainFrame.getTextFieldEditEmployeeIDNumber().getText()),
-                            mainFrame.getTextFieldEditEmployeeLastName().getText(),
-                            mainFrame.getTextFieldEditEmployeeFirstName().getText(),
-                            mainFrame.getTextFieldEditEmployeeMiddleName().getText(),
-                            Double.parseDouble(mainFrame.getTextFieldEditEmployeeSalary().getText()))){
-                        // success: notify user
-                        ViewEmployeeController.getInstance().setViewID(viewID);
-                        ViewEmployeeController.getInstance().showPanel();
-                        
-                    } else{
-                        refreshView();
-                        PopBox.infoBox("Unable to edit", "");
+
+                try {
+
+                    if (!mainFrame.getTextFieldEditEmployeeIDNumber().getText().isEmpty()
+                            && !mainFrame.getTextFieldEditEmployeeLastName().getText().isEmpty()
+                            && !mainFrame.getTextFieldEditEmployeeFirstName().getText().isEmpty()
+                            && !mainFrame.getTextFieldEditEmployeeMiddleName().getText().isEmpty()
+                            && !mainFrame.getTextFieldEditEmployeeSalary().getText().isEmpty()) {
+
+                        if (tryEditEmployee(Integer.parseInt(mainFrame.getTextFieldEditEmployeeIDNumber().getText()),
+                                mainFrame.getTextFieldEditEmployeeLastName().getText(),
+                                mainFrame.getTextFieldEditEmployeeFirstName().getText(),
+                                mainFrame.getTextFieldEditEmployeeMiddleName().getText(),
+                                Double.parseDouble(mainFrame.getTextFieldEditEmployeeSalary().getText()))) {
+                            // success: notify user
+                            ViewEmployeeController.getInstance().setViewID(viewID);
+                            ViewEmployeeController.getInstance().showPanel();
+
+                        } else {
+                            refreshView();
+                            PopBox.infoBox("Unable to edit", "");
+                        }
                     }
+
+                } catch (Exception ex) {
+                    refreshView();
+                    PopBox.infoBox("Unable to edit", "");
                 }
+
             }
 
             @Override
-            public void mousePressed(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {
+            }
 
             @Override
-            public void mouseReleased(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {
+            }
 
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -92,90 +102,86 @@ public class EditEmployeeController implements Listen, PanelChanger{
             }
         });
     }
-    
-    public void setViewID(int id){
+
+    public void setViewID(int id) {
         this.viewID = id;
     }
-    
-    public void refreshView(){
+
+    public void refreshView() {
         try {
             Employee toShow = Employee.getEmployeeByID(this.viewID);
-            mainFrame.getTextFieldEditEmployeeIDNumber().setText(toShow.getIdNumber()+"");
+            mainFrame.getTextFieldEditEmployeeIDNumber().setText(toShow.getIdNumber() + "");
             mainFrame.getTextFieldEditEmployeeFirstName().setText(toShow.getFname());
             mainFrame.getTextFieldEditEmployeeMiddleName().setText(toShow.getMname());
             mainFrame.getTextFieldEditEmployeeLastName().setText(toShow.getLname());
-            mainFrame.getTextFieldEditEmployeeSalary().setText(toShow.getSalary()+"");
-                          
+            mainFrame.getTextFieldEditEmployeeSalary().setText(toShow.getSalary() + "");
+
         } catch (SQLException ex) {
             Logger.getLogger(EditEmployeeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
     @Override
     public void showPanel() {
         refreshView();
         CardLayout layout = (CardLayout) mainFrame.getMainPanelCardPanel().getLayout();
-        layout.show(mainFrame.getMainPanelCardPanel(),PANEL_NAME);
-        
+        layout.show(mainFrame.getMainPanelCardPanel(), PANEL_NAME);
+
         addAuditTrails();
     }
-    
-    private void addAuditTrails(){
+
+    private void addAuditTrails() {
         auditTrails = EmployeeDetailsAuditTrail.getEmployeeDetailsAuditTrailOfEmployee(viewID);
-        
+
         auditTrailPanels = new ArrayList<>();
         int len = auditTrails.size();
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             EditEmployeeChangesPanel panel = new EditEmployeeChangesPanel(auditTrails.get(i).getAttribute(), auditTrails.get(i).getOldValue(), auditTrails.get(i).getNewValue(), auditTrails.get(i).getDate(), auditTrails.get(i).getTime(), AdminModel.getAdminNameByID(auditTrails.get(i).getAdminID()));
             auditTrailPanels.add(panel);
             mainFrame.getPanelEditEmployeeChangesContainer().add(panel);
         }
-        
+
         mainFrame.getPanelEditEmployeeChangesContainer().setPreferredSize(new Dimension(EditEmployeeChangesPanel.PANEL_WIDTH, EditEmployeeChangesPanel.PANEL_HEIGHT * len));
-        
+
     }
-    
+
     // returns true if successful in editing employee, false if there is a similar IDNumber in database
-    private boolean tryEditEmployee(int IDNumber, String lastName, String firstName, String middleName, Double salary){
+    private boolean tryEditEmployee(int IDNumber, String lastName, String firstName, String middleName, Double salary) throws SQLException, ParseException {
         boolean tryEdit = false;
+
         
-        try {    
             // for audit trail
             Employee curEmployee = Employee.getEmployeeByID(viewID);
             EmployeeDetailsAuditTrail audit;
-            
+
             Date dateNow = new SimpleDateFormat("yyyy/MM/dd").parse(CalendarModel.getInstance().getYearToday() + "/" + CalendarModel.getInstance().getMonthToday() + "/" + CalendarModel.getInstance().getDayToday());
-            
+
             DateFormat timeFormat = new SimpleDateFormat("h:mm a");
             Date timeNow = new Date();
             timeFormat.format(timeNow);
-            
-            if(!curEmployee.getFname().equalsIgnoreCase(firstName)){
+
+            if (!curEmployee.getFname().equalsIgnoreCase(firstName)) {
                 audit = new EmployeeDetailsAuditTrail(curEmployee.getEmpID(), EmployeeDetailsAuditTrail.ATTRIBUTE_FIRST_NAME, curEmployee.getFname(), firstName, dateNow, new java.sql.Time(timeNow.getTime()), NavigationController.getInstance().getAdmin().getId());
                 audit.addAuditTrail();
             }
-            if(!curEmployee.getLname().equalsIgnoreCase(lastName)){
+            if (!curEmployee.getLname().equalsIgnoreCase(lastName)) {
                 audit = new EmployeeDetailsAuditTrail(curEmployee.getEmpID(), EmployeeDetailsAuditTrail.ATTRIBUTE_LAST_NAME, curEmployee.getLname(), lastName, dateNow, new java.sql.Time(timeNow.getTime()), NavigationController.getInstance().getAdmin().getId());
                 audit.addAuditTrail();
             }
-            if(!curEmployee.getMname().equalsIgnoreCase(middleName)){
+            if (!curEmployee.getMname().equalsIgnoreCase(middleName)) {
                 audit = new EmployeeDetailsAuditTrail(curEmployee.getEmpID(), EmployeeDetailsAuditTrail.ATTRIBUTE_MIDDLE_NAME, curEmployee.getMname(), middleName, dateNow, new java.sql.Time(timeNow.getTime()), NavigationController.getInstance().getAdmin().getId());
                 audit.addAuditTrail();
             }
-            if(curEmployee.getSalary() != salary){
+            if (curEmployee.getSalary() != salary) {
                 audit = new EmployeeDetailsAuditTrail(curEmployee.getEmpID(), EmployeeDetailsAuditTrail.ATTRIBUTE_SALARY, String.valueOf(curEmployee.getSalary()), String.valueOf(salary), dateNow, new java.sql.Time(timeNow.getTime()), NavigationController.getInstance().getAdmin().getId());
                 audit.addAuditTrail();
             }
-            
+
             Employee.editEmployee(IDNumber, firstName, lastName, middleName, salary);
             tryEdit = true;
-            
-        } catch (SQLException | ParseException ex) {
-            Logger.getLogger(EditEmployeeController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
+
+
+
         // logic here
         return tryEdit;
     }
